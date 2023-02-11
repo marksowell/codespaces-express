4c5ca74acc4f4d0e425fc0cceefd1f6ddb50daa8
Mark Sowell <mark@marksowell.com>
2023-02-11 23:10:52 +0000

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const projectName = process.env.PROJECTNAME
 
 app.get('/', (req, res) => {
   res.send(`Hello World3! - ${projectName}`)
